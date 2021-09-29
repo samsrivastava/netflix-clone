4832a4a56b6211e8dcaff36038fbe3f39f9eeca1
samsrivastava <samarth.19652@knit.ac.in>
2021-09-29 19:59:31 +0530

    Fetched posters and styled it

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <h1>Samarth and Anas on Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
+      {/* <h1>Samarth and Anas on Netflix Clone</h1> */}
+      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
       <Row title="Trending now" fetchURL = {requests.fetchTrending}/>
+      <Row title="TopRated" fetchURL = {requests.fetchTopRated}/>
+      <Row title="Popular" fetchURL = {requests.fetchPopular}/>
+      <Row title="Horror Movies" fetchURL = {requests.fetchHorrorMovies}/>
 
 
 
--- src/Row.js
+++ src/Row.js
@@ -1,24 +1,41 @@
 import React, { useState, useEffect } from 'react'
 
 import axios from './axios';
+import "./Row.css"
 
-function Row( { title, fetchURL }) {
-    // const [movie, setMovies] = useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/"
 
+function Row( { title, fetchURL, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
          async function fetchData(){
              const request = await axios.get(fetchURL);
-             console.log(request);
-            //  setMovies(request.data.results);
+            //  console.log(request.data.results);
+             setMovies(request.data.results);
              return request;
          }
          fetchData();
     }, [fetchURL]);
 
+    console.log(movies);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+
+            <div className="row__posters">
+
+            {movies.map(movie =>(
+                <img 
+                key ={movie.id}
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                  alt={movie.name} />
+            ))}
+            </div>
+            
+            
         </div>
     )
 }
